fedcb6585805c08c5d7aea20c901b0d531a1df22
Jeroen van der Heijden <joente@gmail.com>
2017-03-22 21:53:02 +0100

    Added debug

---
+++ TestDebug/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../main.c \
+../qpack.c 
+
+OBJS += \
+./main.o \
+./qpack.o 
+
+C_DEPS += \
+./main.d \
+./qpack.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	gcc -DTEST_ -I../ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
--- main.c
+++ main.c
@@ -4,16 +4,23 @@
  *  Created on: Mar 22, 2017
  *      Author: joente
  */
-
+#ifdef TEST_
 
 #include <qpack.h>
+#include <stdio.h>
 
 int main(void)
 {
     qp_packer_t * packer;
 
+    puts("Start QPack library test...");
+
     packer = qp_packer_create(QP_SUGGESTED_SIZE);
-    puts("This is a shared library test...");
-    foo();
+    qp_packer_destroy(packer);
+
+    puts("Finished QPack library test!");
+
     return 0;
 }
+
+#endif /* TEST_ */
