774205fdcb48a899ae5858e4524b124030afeecc
Jeroen van der Heijden <jeroen@transceptor.technology>
2017-09-08 12:56:20 +0200

    Fixed bug in length check for unpacking strings

--- qpack.h
+++ qpack.h
@@ -10,7 +10,7 @@
 
 #define QP_VERSION_MAJOR 0
 #define QP_VERSION_MINOR 10
-#define QP_VERSION_PATCH 5
+#define QP_VERSION_PATCH 6
 
 #define QP_STRINGIFY(num) #num
 #define QP_VERSION_STR(major,minor,patch)   \
